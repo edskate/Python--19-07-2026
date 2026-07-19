bb50ae2f28c23e172f7708cb254fc5cfc4aec3f7
Edy_97_2024 <edskateicevida@gmail.com>
2026-07-19 16:45:47 -0300

    atualizado

--- loops.py
+++ loops.py
@@ -2,5 +2,17 @@
 # contador = 1
 
 # while contador <= 10:
-    # print(f"Contagem atual: {contador}")
-    # contador = contador + 1
+ # print(f"Contagem atual: {contador}")
+ # contador = contador + 1
+ 
+ 
+senha = "calabreso123"
+tentativa_senha = ""
+
+while tentativa_senha != senha:
+    tentativa_senha = input("Digite sua senha: ")
+
+    if tentativa_senha != senha:
+        print("👉 Senha incorreta, tente novamente.")
+    else:
+        print("👉 Acertou!")
