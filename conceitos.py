6df2b6c9901cfe4d19ba1bbf63ecf057a74bac99
Edy_97_2024 <edskateicevida@gmail.com>
2026-07-19 13:32:06 -0300

    add

---
+++ conceitos.py
@@ -0,0 +1,29 @@
+# nome = "lan code"
+# print("🍫 ola, " + nome)
+
+
+
+# nome = input("qual é o seu nome? ")
+# print(nome)
+
+# num1 = int(input("Digite um número: "))
+# print(f"O resultado da soma entre {num1} e {num2} e  {resultado_soma}")
+
+
+# idade = int(input("qual e a sua idade: "))
+# if idade > 17:
+    # print("👉  Ta liberado ")
+# print("👉 fim do programa")
+
+# cores = ("vermelho", "verde", "azul")
+# cores[1] ="amarelo"  
+
+
+# minhas_tuplas = [("maca", "banana"), ("verde", "vermeho")]
+# print(minhas_tuplas[1][0])
+
+# dados = {"nome":"lan code", "idade":22}
+# dados["idade"] = 22
+# print(dados)
+
+
